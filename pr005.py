7cb76a110b89e3730aa96df98e09c0ef3d896b18
Sam Rao <samrao1997@gmail.com>
2020-06-06 21:35:09 -0700

    problem 5 -- basically least common mult

--- pr005.py
+++ pr005.py
@@ -1,9 +1,50 @@
-""" 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+## 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+## What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
 
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? """
+# This reduces to finding the least common multiple.
+
+def least_common_mult(arr):
+    m = max(arr)
+    p = list_primes(m)
+
+    N = 1
+
+    for i in range(len(p)):
+        mult = p[i]
+        # print(mult)
+        # print(N)
+        while (mult * p[i] < m):
+            mult = mult * p[i]
+        # print(mult)
+        N = N * mult
+        # print(N)
+
+    
+    return N
 
 
-def do_nothing():
-    return
+def list_primes(n):
+    """
+    This returns a list of the prime numbers less than n
+    """
 
-print("Hello")
+    if n == 1: return []
+
+    arr = []
+
+    for i in range(2,n+1):
+        if (is_prime(i)):
+            arr.append(i)
+
+    return arr
+
+
+def is_prime(num):
+    for i in range(2,(num // 2) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+test = range(1,11)
+print(least_common_mult(test))
