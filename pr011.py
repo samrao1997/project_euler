f4df0fa48a4c0295d388965329c725b86df0a7aa
Sam Rao <samrao1997@gmail.com>
2020-06-08 20:11:00 -0700

    edit permissions

--- pr011.py
+++ pr011.py
@@ -109,10 +109,11 @@ grid = """08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
 01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"""
 
-grid = grid.split("\n")
+
 
 ## Some preprocessing to make grid of string into 2D array of ints
 # print(grid)
+grid = grid.split("\n")
 for i in range(len(grid)):
     grid[i] = grid[i].split(" ")
     for j in range(len(grid[i])):
