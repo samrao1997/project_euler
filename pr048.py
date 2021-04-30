40771e1194c94a0e900a3476ef9f5a0d69f74db0
Samuel Rao <samrao1997@gmail.com>
2021-04-29 22:42:43 -0400

    solved problem 48

---
+++ pr048.py
@@ -0,0 +1,12 @@
+"""
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+"""
+
+
+def sol():
+    total = 0
+    for i in range(1, 1001):
+        total += i ** i
+    return total
