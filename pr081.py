9d74540e645dce3da8b4d9fe8f53a9e3e2dd31ab
Samuel Rao <samrao1997@gmail.com>
2021-06-07 11:33:12 -0400

    solved problem 81

---
+++ pr081.py
@@ -0,0 +1,34 @@
+"""
+n the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by only moving to the right and down, is indicated in bold red and is equal to 2427.
+
+ 
+(
+131
+673	234	103	18	
+201
+96
+342
+965	150	630	803	
+746
+422
+111	537	699	497	
+121
+956	805	732	524	
+37
+331
+ 
+)
+Find the minimal path sum from the top left to the bottom right by only moving right and down in matrix.txt (right click and "Save Link/Target As..."), a 31K text file containing an 80 by 80 matrix.
+"""
+
+
+m = [list(map(int, row.split(","))) for row in open("p081_matrix.txt").readlines()]
+c, r = len(m), len(m[0])
+
+for i in range(1, c):
+    for j in range(1, r):
+        m[i][0] += m[i - 1][0]
+        m[0][j] += m[0][j - 1]
+        m[i][j] += min(m[i - 1][j], m[i][j - 1])
+
+print("Minimal path sum in", r, "by", c, "matrix =", m[-1][-1])
