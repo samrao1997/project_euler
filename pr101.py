ae7415835b2f80277b54976a27f673d14f680353
Samuel Rao <samrao1997@gmail.com>
2021-07-01 16:06:51 -0400

    solved problem 101

---
+++ pr101.py
@@ -0,0 +1,13 @@
+#Project Euler Problem 101
+
+k = 10
+u = lambda n: 1 - n + n**2 - n**3 + n**4 - n**5 + n**6 - n**7 + n**8 - n**9 + n**10
+
+def tld(x):
+	return [x[i] - x[i-1] for i in range(1, len(x))]
+
+seq = [list(map(u, range(1, k+1)))]
+for i in range(k):
+	seq += [tld(seq[-1])]
+
+print("Sum of FITs for the BOPs", sum(sum(seq, [])))
