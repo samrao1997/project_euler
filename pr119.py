4c656514c03d5d3dc92392c1c1d688d0329eaff8
Samuel Rao <samrao1997@gmail.com>
2021-08-04 11:44:18 -0400

    solved problem 119

---
+++ pr119.py
@@ -0,0 +1,12 @@
+def sum_of_digits(n):
+  return sum(map(int, str(n)))
+
+a = []
+n = 30
+for b in range(2, 100):
+    for e in range(2, 10):
+        p = b ** e
+        if sum_of_digits(p) == b: a.append(p)
+
+a.sort()
+print("Answer to PE119 = a(%d) =" % n, a[n-1])
