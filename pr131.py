1f294eb4d2fb73b4f7463ea1837d7abeb7774474
Samuel Rao <samrao1997@gmail.com>
2021-08-20 12:16:14 -0700

    solved problem 131

---
+++ pr131.py
@@ -0,0 +1,30 @@
+def sqrt(x):
+	assert x >= 0
+	i = 1
+	while i * i <= x:
+		i *= 2
+	y = 0
+	while i > 0:
+		if (y + i)**2 <= x:
+			y += i
+		i //= 2
+	return y
+
+def is_prime(x):
+	if x <= 1:
+		return False
+	elif x <= 3:
+		return True
+	elif x % 2 == 0:
+		return False
+	else:
+		for i in range(3, sqrt(x) + 1, 2):
+			if x % i == 0:
+				return False
+		return True
+
+
+result = 0
+for i in range(1, 577):
+    if is_prime((i + 1) * (i+1) * (i + 1)- i *i*i): result = result +1
+print(result)
